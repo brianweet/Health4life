c0efcfff1f0f6db322f5df55c78cfb8d4a6fa136
brianweet <brian_weeteling@hotmail.com>
2013-10-25 12:02:40 +0200

    Added basic pages and deploying to azure

---
+++ Health4life/Controllers/UserAreaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Health4life.Controllers
+{
+    public class UserAreaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
